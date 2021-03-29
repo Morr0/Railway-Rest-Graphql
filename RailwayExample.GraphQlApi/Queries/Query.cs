555be5954236e3c1bc9c0ec06dc6d67ab12b5174
Rami Hikmat <rami@ramihikmat.net>
2021-03-30 00:49:50 +1100

    Query GraphQl

---
+++ RailwayExample.GraphQlApi/Queries/Query.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using RailwayExample.Core;
+using RailwayExample.Core.Models;
+
+namespace RailwayExample.GraphQlApi.Queries
+{
+    public class Query
+    {
+        public Train GetTrain([Required] string id)
+        {
+            return Data.Trains.FirstOrDefault(x => x.Id == id);
+        }
+
+        public IEnumerable<Train> GetTrains()
+        {
+            return Data.Trains;
+        }
+
+        public Station GetStation([Required] string id)
+        {
+            return Data.Stations.FirstOrDefault(x => x.Id == id);
+        }
+
+        public IEnumerable<Station> GetStations()
+        {
+            return Data.Stations;
+        }
+    }
+}
